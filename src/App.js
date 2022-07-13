185464429c174b8f12b41553ca601fdde7b7387d
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 09:54:03 +0530

    init routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 export default function App() {
   return (
-    <div>
-      APP
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
