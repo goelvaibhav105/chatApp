77c35c84e1c8737758def9de4d993886c3d08ebc
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 13:25:23 +0530

    backend for login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const {
-  register,
+  register,login
 } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 
 router.post("/register", register);
+router.post("/login", login);
 
 
 module.exports = router;
