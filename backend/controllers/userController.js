d68e1a2f549df216002fffaf62d44f2d80caf592
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 12:19:36 +0530

    insturcton added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,35 @@
 const User = require("../models/userModel");
+// usinh bcrypt for password 
 const bcrypt = require("bcrypt");
 
 
 module.exports.register = async (req, res, next) => {
-   console.log(req.body);
+    // chk this if u wanna see api is working fine or not 
+  // console.log(req.body)
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    // checking userName is there or not 
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+      // checking email is there or not 
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+      // hashing password 
+    const hashedPassword = await bcrypt.hash(password, 10);
+     // using create api creating password 
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    // delting user password for complete transprency 
+    delete user.password;
+    // returning user object as json 
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
 };
 
