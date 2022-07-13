f3f354d3f946a1881450793089a4c67746a298e3
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 11:38:37 +0530

    validation and register func added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,63 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import styled from 'styled-components'
 import { useNavigate, Link } from "react-router-dom";
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logo.svg';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
+  const [values, setValues] = useState({
+    username : "",
+    email : "",
+    password : "",
+    confirmPassword : "",
+  })
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
   const handleSubmit = (event) =>{
      event.preventDefault();
-     alert("form")
+     handleValidation()
   }
 
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+
+
+  if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
+
+
   const handleChange = (event) =>{
-   
+   setValues({...values,[event.target.name]:event.target.value});
  }
 
   return (
@@ -45,12 +92,13 @@ function Register() {
           name="confirmPassword"
           onChange={(e) =>{handleChange(e)}}
          />
-      </form>
-      <button type='submit'>Create User</button>
+          <button type='submit'>Create User</button>
          <span>
             Already have an account ? <Link to="/login">Login.</Link>
           </span>
+      </form>
     </FormContainer>
+    <ToastContainer/>
     </>
   )
 }
