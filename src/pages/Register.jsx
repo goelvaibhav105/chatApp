97a39cc7b06b0e8e25084fe7496f4655b33c6363
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 12:06:24 +0530

    register api call from frontend func

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,8 +4,11 @@ import { useNavigate, Link } from "react-router-dom";
 import Logo from '../assets/logo.svg';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
+  const navigate = useNavigate();
   const [values, setValues] = useState({
     username : "",
     email : "",
@@ -20,9 +23,28 @@ function Register() {
     theme: "dark",
   };
 
-  const handleSubmit = (event) =>{
+  const handleSubmit = async (event) =>{
      event.preventDefault();
-     handleValidation()
+     if(handleValidation()){
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+        navigate("/");
+      }
+  
+     }
   }
 
   const handleValidation = () => {
