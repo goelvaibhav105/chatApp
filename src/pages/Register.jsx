c0c22b4dd975a3504c2a3e636b3665449674da97
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 10:57:36 +0530

    Register C init

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useNavigate, Link } from "react-router-dom";
 
 function Register() {
+  const handleSubmit = (event) =>{
+     event.preventDefault();
+     alert("form")
+  }
+
+  const handleChange = (event) =>{
+   
+ }
+
   return (
-    <div>Register</div>
+    <>
+    <FormContainer>
+      <form onSubmit={(event)=>{handleSubmit(event)}}>
+         <div className="brand">
+          <img src="" alt="" />
+          <h1>Chat App </h1>
+         </div>
+         <input 
+          type="text" 
+          placeholder='Username'
+          name="username"
+          onChange={(e) =>{handleChange(e)}}
+         />
+          <input 
+          type="email" 
+          placeholder='Email'
+          name="email"
+          onChange={(e) =>{handleChange(e)}}
+         />
+          <input 
+          type="password" 
+          placeholder='Password'
+          name="password"
+          onChange={(e) =>{handleChange(e)}}
+         />
+          <input 
+          type="password" 
+          placeholder='Confirm Password'
+          name="confirmPassword"
+          onChange={(e) =>{handleChange(e)}}
+         />
+      </form>
+      <button type='submit'>Create User</button>
+         <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+    </FormContainer>
+    </>
   )
 }
+
+const FormContainer = styled.div``
 
 export default Register
