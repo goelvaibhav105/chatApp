3ec8a8914342fde523ba04593bae249a52f6b2b1
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-13 16:23:19 +0530

    welcome page setup

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
 export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(()=>{
+    const getUserFromLC = async () => {
+      setUserName(
+        await JSON.parse(
+          localStorage.getItem('chat-app-user')
+        ).username
+      );
+    }
+    getUserFromLC();
+  }, []);
   return (
-    <div>Welcome</div>
-  )
+    <Container>
+      <img src={Robot} alt="" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
