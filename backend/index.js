8b4878789d1201ee45d9c31343066c5d4d85f4a3
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-15 12:35:29 +0530

    socket.io backend func

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes')
 const messageRoutes = require('./routes/messageRoutes')
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -27,6 +28,32 @@ mongoose
 const server = app.listen(process.env.PORT,()=>{
     console.log(`Servern Started on PORT ${process.env.PORT}`)
 })
+
+// defining origin 
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
+
+
 
 
 
