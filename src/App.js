325ee630ab6a364642433e0ff582fc6b70c7dd1d
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-19 09:24:38 +0530

    Some required changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SetAvatar from "./pages/SetAvatar";
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/setavatar" element={<SetAvatar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Chat />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
